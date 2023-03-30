0fc49528c6753ff30a1d5187d10fef53ed80dc2c
Varshakalva <kalvakuntla.varsha@gmail.com>
2023-03-29 19:00:39 -0500

    done changes to bonus compute cal

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
+var computeRouter = require('./routes/compute');
 
 var app = express();
 
@@ -23,11 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
+app.use('/compute',computeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/compute.js
+++ routes/compute.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 
@@ -7,14 +6,15 @@ var router = express.Router();
 let x;
 let y;
 router.get('/', function(req, res, next) {
-  //res.render('computation', { title: 'Varsha Kalvakuntla' });
+  // res.send('computation', { title: 'Varsha Kalvakuntla TOPPER' });
   
-if(req.query.x ==undefined ) {
+if(req.query.x == undefined ) {
   x=Math.floor(Math.random()*10);
   y=Math.fround(Math.random());
-let y1=Math.atan(x,y);
+let y1=Math.atan(x+y);
 let y2=Math.exp(y);
-let y3=Math.exmp1(x);
+let y3 = 0;
+// let y3=Math.exmp1(x);
   res.send('Math.atan() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.exp() applied to '+y+' is '+y2+"<br>"+'Math.exmp1()  applied to '+x+' is'+y3); 
 
 }
