18c4c4963eb0681615d45fe27a0e910124d6d4ca
Varshakalva <kalvakuntla.varsha@gmail.com>
2023-03-29 21:07:50 -0500

    did changes to compute page bonus

--- routes/compute.js
+++ routes/compute.js
@@ -6,22 +6,21 @@ var router = express.Router();
 let x;
 let y;
 router.get('/', function(req, res, next) {
-  // res.send('computation', { title: 'Varsha Kalvakuntla' });
+  //res.render('compute', { title: 'Maheshwar Punyam Anand' });
   
-if(req.query.x == undefined ) {
+if(req.query.x ==undefined ) {
   x=Math.floor(Math.random()*10);
   y=Math.fround(Math.random());
-let y1=Math.atan(x+y);
+let y1=Math.atan(x);
 let y2=Math.exp(y);
-let y3 = 0;
-// let y3=Math.exmp1(x);
-  res.send('Math.atan() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.exp() applied to '+y+' is '+y2+"<br>"+'Math.exmp1()  applied to '+x+' is'+y3); 
+let y3=Math.expm1(x);
+  res.send('Math.atan() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.exp() applied to '+y+' is '+y2+"<br>"+'Math.expm1()  applied to '+x+' is'+y3); 
 
 }
 else{
 
   x = req.query.x;
-  let y1=Math.atan()(x,x);
+  let y1=Math.atan(x);
   let y2=Math.exp(x);
   let y3=Math.expm1(x);
 
